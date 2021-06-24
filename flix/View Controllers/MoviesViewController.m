49d45c9d2abd2644e5b7dfc5a03c548a3dbce3d1
constance19 <67133685+constance19@users.noreply.github.com>
2021-06-23 22:43:07 -0700

    added 7, 8
    issue: UIActivity indicator not animating in the simulator

--- flix/View Controllers/MoviesViewController.m
+++ flix/View Controllers/MoviesViewController.m
@@ -9,25 +9,56 @@
 #import "MovieCell.h"
 #import "UIImageView+AFNetworking.h"
 #import "DetailsViewController.h"
+#import "Reachability.h"
 
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray *movies;
 @property (nonatomic, strong) UIRefreshControl *refreshControl;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+//@property (nonatomic) Reachability *internetReachability;
 
 @end
 
 @implementation MoviesViewController
 
+// Checks if we have an internet connection or not
+- (void)testInternetConnection
+{
+    self.internetReachability = [Reachability reachabilityWithHostname:@"www.facebook.com"];
+
+    // Internet is reachable
+    self.internetReachability.reachableBlock = ^(Reachability*reach)
+    {
+        // Update the UI on the main thread
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"Yayyy, we have the interwebs!");
+        });
+    };
+
+    // Internet is not reachable
+    self.internetReachability.unreachableBlock = ^(Reachability*reach)
+    {
+        // Update the UI on the main thread
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"Someone broke the internet :(");
+        });
+    };
+
+    [self.internetReachability startNotifier];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.activityIndicator startAnimating];
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     
     [self fetchMovies];
+    [self.activityIndicator stopAnimating];
     
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
@@ -39,10 +70,29 @@
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        // Handle network error with alert message
            if (error != nil) {
+               UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Cannot Get Movies"
+                                                                                          message:@"The internet connection appears to be offline."
+                                                                                   preferredStyle:(UIAlertControllerStyleAlert)];
+               
+               UIAlertAction *tryAction = [UIAlertAction actionWithTitle:@"Try Again"
+                                                                   style:UIAlertActionStyleCancel
+                                                                 handler:^(UIAlertAction * _Nonnull action) {
+                                                                        // handle try again response here. Doing nothing will dismiss the view.
+                                                                 }];
+               // add the try again action to the alertController
+               [alert addAction:tryAction];
+               
+               [self presentViewController:alert animated:YES completion:^{
+                   [self fetchMovies];
+               }];
+               
                NSLog(@"%@", [error localizedDescription]);
-           }
-           else {
+               
+            // No network error
+           } else {
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
                
                NSLog(@"%@", dataDictionary);
@@ -58,6 +108,7 @@
                // TODO: Store the movies in a property to use elsewhere
                // TODO: Reload your table view data
            }
+        
         [self.refreshControl endRefreshing];
        }];
     [task resume];
